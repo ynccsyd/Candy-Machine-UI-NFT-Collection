0e5c620b086d36cff1469cd53d4a3fcd797cab06
ynccsyd <muhibbi_10_@hotmaile.com>
2022-12-14 23:19:49 +0300

    Added gitignore

--- commands.sh
+++ commands.sh
@@ -4,13 +4,14 @@ solana-keygen new --outfile ./wallet.json
 #Save this seed phrase and your BIP39 passphrase to recover your new keypair:
 #upper effort pull vote violin female delay fault robot welcome risk aerobic
 solana config set --keypair ./wallet.json
-#solana config set --url https://metaplex.devnet.rpcpool.com/
+solana config set --url https://metaplex.devnet.rpcpool.com/
 #Config File: C:\Users\sydyncc\.config\solana\cli\config.yml
 #RPC URL: https://metaplex.devnet.rpcpool.com/ 
 ##WebSocket URL: wss://metaplex.devnet.rpcpool.com/ (computed)
 #Keypair Path: ./wallet.json
 #Commitment: confirmed
-
+sugar launch
+sugar mint
 
 Candy machine ID: 35Awjr1Mrb8JSNXtZLrf5mpm5yruSNh2z5zPqmq3yZqL
 
